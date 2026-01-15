fc38e874dbd476ece3e74aa2d9555ca94346ba11
Iara Braga <iara@incibrasil.com.br>
2026-01-15 10:11:36 -0300

    feat: using seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Avatar;
+use App\Models\Level;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -17,9 +19,27 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+        Level::create([
+            'name' => 'Level 1',
+            'image' => 'images/level1.png',
+            'needed_score' => 0,
+            'created_at' => now()
+        ]);
+
+        User::create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'teste@gmail.com',
+            'password' => '123',
+            'sequence' => 0,
+            'general_score' => 0,
+            'weekly_score' => 0,
+            'level_id' => Level::where('name', 'Level 1')->first()->id,
+            'created_at' => now()
+        ]);
+
+        Avatar::create([
+            'image' => 'images/avatar-default.png',
+            'created_at' => now()
         ]);
     }
 }
