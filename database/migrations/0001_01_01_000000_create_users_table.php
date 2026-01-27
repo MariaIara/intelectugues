c5afe32a4214d4b0df90b448318dca1fbcdc69eb
Iara Braga <iara@incibrasil.com.br>
2026-01-27 20:51:20 -0300

    feat: add new user field

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,7 +19,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->integer('sequence')->default(0);
+            $table->integer('general_sequence')->default(0);
+            $table->integer('weekly_sequence')->default(0);
             $table->integer('general_score')->default(0);
             $table->integer('weekly_score')->default(0);
             $table->foreignIdFor(Avatar::class)->nullable();
